1534f594cb3d5e4339519dd56cfa7085c7e9127f
Miles Breslin <milesbreslin@gmail.com>
2020-06-05 21:21:31 -0700

    Allow different prompt sizes

--- bagnoster.bash
+++ bagnoster.bash
@@ -15,15 +15,22 @@ _get_prompt_color() {
 
 _base_prompt() {
     local path_rep='\w'                                                         # Default Let the Shell Handle its own path
-    ((COLUMNS < 50)) && path_rep="$(basename "$PWD")"
+    if ((PROMPT_SIZE>=2)) ; then
+        path_rep='$'
+    elif ((PROMPT_SIZE>=1)) || ((COLUMNS<"${#PWD}"*2)) ; then
+        path_rep="$(basename "$PWD")"
+    fi
 
     if ! git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null; then       # Is Not Git Directory
         PS1="\[\033[${path_color};30m\] $path_rep \[\033[0;$((path_color-10))m\]$_POWERLINE_ARROW\[\033[0m\] "    # Make it all a string
     else
-        local Git_branch="$(basename "$(git symbolic-ref HEAD 2>/dev/null)")"
+        local Git_branch=""
+        if ((PROMPT_SIZE==0)) ; then
+            Git_branch="$(basename "$(git symbolic-ref HEAD 2>/dev/null)") "
+        fi
         local Git_color=43
         [ -z "$(git status -s 2> /dev/null)" ] && Git_color=42                  # Is clean working tree
-        PS1="\[\033[${path_color};30m\] $path_rep \[\033[${Git_color};$((path_color-10))m\]$_POWERLINE_ARROW\[\033[${Git_color};30m\] $_POWERLINE_BRANCH$Git_branch \[\033[0;$((Git_color-10))m\]$_POWERLINE_ARROW\[\033[0m\] "    # Make it all a string, relying on \w for directory
+        PS1="\[\033[${path_color};30m\] $path_rep \[\033[${Git_color};$((path_color-10))m\]$_POWERLINE_ARROW\[\033[${Git_color};30m\] $_POWERLINE_BRANCH$Git_branch\[\033[0;$((Git_color-10))m\]$_POWERLINE_ARROW\[\033[0m\] "    # Make it all a string, relying on \w for directory
     fi
     ((COLUMNS < 50)) && PS1="$PS1"$'\n'
 }
@@ -39,13 +46,14 @@ _pre_newline() {
 
 _zsh_newline() {
     unset PROMPT_SP                                                             # Detect whether or not the command has a new line ending
-    for ((i = 1; i<= $COLUMNS + 52; i++ )); do
+    for ((i = 1; i<= $COLUMNS + 52; i++ )); do                                  # Will mangle terminal on resize
         PROMPT_SP+=' ';
     done                                                                        # Credit to Dennis Williamson on serverfault.com
     PS1='\[\e[7m%\e[m\]${PROMPT_SP: -$COLUMNS+1}\015'"$PS1"
 }
 
 _pre_hostname() {
+    ((PROMPT_SIZE==0)) || return 0
     local hostname_color=47
     local hostname="$(</etc/hostname)"
     local hostname="${hostname/.*}"
@@ -62,4 +70,5 @@ _prompt() {
     _pre_newline
 }
 
+PROMPT_SIZE=0
 PROMPT_COMMAND=_prompt

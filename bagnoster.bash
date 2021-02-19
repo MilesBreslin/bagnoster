bd3825e4b145fd788c1535d677d685e676e432f5
Miles Breslin <milesbreslin@gmail.com>
2021-02-18 16:57:48 -0800

    Major reorganization. Add nix-shell detection. Allow manual path symbolic shrinking.

--- bagnoster.bash
+++ bagnoster.bash
@@ -1,38 +1,58 @@
 #!/usr/bin/env bash
 
 if [ "$TERM" != "linux" ]; then
-    export _POWERLINE_ARROW=""
-    export _POWERLINE_BRANCH=" "                                                # Needs to have space after this
+    export _POWERLINE_ARROW=""     # Needs to have space after this
+    export _POWERLINE_BRANCH=""
 fi
 
-_get_prompt_color() {
-    local ERR="$?"
-    local path_color=44                                                         # Default Blue
-    [ "$(id -u)" == "0" ] && path_color=45                                      # Is Root? Then White
-    [ "$ERR" != "0" ] && path_color=41                                          # Command Retuned error? Then Red
-    echo "$path_color"
+_set_pretty_prompt() {
+    unset PS1
+    local padding="$1"
+    shift
+    while [ -n "$1" ] ; do
+        local bg_color="$1"
+        local primary_color="$2"
+        local text="$3"
+        local transition_color=0
+        [ -n "$4" ] && transition_color="$(("$4"))"
+        shift ; shift ; shift
+        PS1+="\[\033[${bg_color};${primary_color}m\]${padding}${text}${padding}\[\033[${transition_color};$((bg_color-10))m\]$_POWERLINE_ARROW"
+    done
+    PS1+="\[\033[0m\]${padding}"
+}
+
+_append_to_prompt() {
+    PRETTY_PROMPT=("${PRETTY_PROMPT[@]}" "$1" "$2" "$3")
 }
 
 _base_prompt() {
-    local path_rep='\w'                                                         # Default Let the Shell Handle its own path
-    if ((PROMPT_SIZE>=2)) ; then
-        path_rep='$'
-    elif ((PROMPT_SIZE>=1)) || ((COLUMNS<"${#PWD}"*2)) ; then
-        path_rep="$(basename "$PWD")"
-    fi
-
-    if ! git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null; then       # Is Not Git Directory
-        PS1="\[\033[${path_color};30m\] $path_rep \[\033[0;$((path_color-10))m\]$_POWERLINE_ARROW\[\033[0m\] "    # Make it all a string
+    local ERR="${1:-0}"
+    local path_color=44                                                     # Default Blue
+    [[ "$PATH" =~ ^/nix/store ]] && path_color=46                           # Compressed Nix Store signal
+    [ "$(id -u)" == "0" ] && path_color=45                                  # Is Root? Then White
+    [ -n "${PROMPT_SET_COLOR//[^0-9]}" ] && path_color="$PROMPT_SET_COLOR"  # Overriden prompt color?
+    [ "$ERR" != "0" ] && path_color=41                                      # Command Retuned error? Then Red
+    if [ -n "$PROMPT_PATH" ] ; then
+        _append_to_prompt "$path_color" 30 "${PWD/"${PROMPT_PATH}"/"${PROMPT_SET_PATH:-!}"}"
+    elif ((PROMPT_SIZE==2)) ; then
+        _append_to_prompt "$path_color" 30 "\w"
     else
+        _append_to_prompt "$path_color" 30 "$(basename "$PWD")"
+    fi
+}
+
+_git_prompt() {
+    if git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null; then     # Is Not Git Directory
         local Git_branch=""
-        if ((PROMPT_SIZE==0)) ; then
-            Git_branch="$(basename "$(git symbolic-ref HEAD 2>/dev/null)") "
+        local branch_symbol="${_POWERLINE_BRANCH}"
+        if ((PROMPT_SIZE==2)) ; then
+            Git_branch="$(basename "$(git symbolic-ref HEAD 2>/dev/null)")"
+            [ -n "${_POWERLINE_BRANCH}" ] && branch_symbol="${_POWERLINE_BRANCH} "
         fi
         local Git_color=43
-        [ -z "$(git status -s 2> /dev/null)" ] && Git_color=42                  # Is clean working tree
-        PS1="\[\033[${path_color};30m\] $path_rep \[\033[${Git_color};$((path_color-10))m\]$_POWERLINE_ARROW\[\033[${Git_color};30m\] $_POWERLINE_BRANCH$Git_branch\[\033[0;$((Git_color-10))m\]$_POWERLINE_ARROW\[\033[0m\] "    # Make it all a string, relying on \w for directory
+        [ -z "$(git status -s 2> /dev/null)" ] && Git_color=42              # Is clean working tree
+        _append_to_prompt "${Git_color}" 30 "${branch_symbol}${Git_branch}"
     fi
-    ((COLUMNS < 50)) && PS1="$PS1"$'\n'
 }
 
 _reload_history() {
@@ -53,22 +73,43 @@ _zsh_newline() {
 }
 
 _pre_hostname() {
-    ((PROMPT_SIZE==0)) || return 0
+    ((PROMPT_SIZE>=2)) || return 0;
     local hostname_color=47
-    local hostname="$(</etc/hostname)"
+    local hostname="$(</proc/sys/kernel/hostname)"
     local hostname="${hostname/.*}"
-    PS1="\[\033[${hostname_color};30m\] $hostname \[\033[${path_color};$((hostname_color-10))m\]$_POWERLINE_ARROW$PS1"
+    _append_to_prompt 47 30 "$hostname"
+}
+
+_nix_shell() {
+    ((PROMPT_SIZE>=2)) || return 0
+    if [[ "$PATH" =~ ^/nix/store ]] ; then
+        _append_to_prompt 42 30 "$(<<< "$PATH" sed 's,:.*,,;s,^/nix/store/[a-z0-9]*-,,;s,-.*,,;s,/bin$,,')"
+    fi
 }
 
 _prompt() {
-    local path_color="$(_get_prompt_color)"
-    _base_prompt
-    _reload_history
+    local ERR="$?"
+    local PRETTY_PROMPT=()
+
+    # Calculate prompt size if not set. Larger number is larger size
+    if [ -z "$PROMPT_SIZE" ] ; then
+        local PROMPT_SIZE=2
+        ((COLUMNS<75)) && PROMPT_SIZE="1"
+        ((COLUMNS<50)) && PROMPT_SIZE="0"
+    fi
+
+    local padding=" "
+    ((PROMPT_SIZE<2)) && padding=""
+
+    _nix_shell
     if [ -n "$SSH_CONNECTION" ] || [ -n "$TMUX" ] || [ -n "$SUDO_COMMAND" ]; then
         _pre_hostname
     fi
+    _base_prompt "$ERR"
+    _git_prompt
+    _set_pretty_prompt "$padding" "${PRETTY_PROMPT[@]}"
+    _reload_history
     _pre_newline
 }
 
-PROMPT_SIZE=0
 PROMPT_COMMAND=_prompt

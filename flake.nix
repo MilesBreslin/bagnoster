a7d7b296725c45729f6c86a3b2a5ee29e61b94dd
Miles Breslin <milesbreslin@gmail.com>
2025-12-14 00:02:48 -0800

    Fix aarch64, use coreutils, and use nixpkgs 25.11 directly

--- flake.nix
+++ flake.nix
@@ -1,27 +1,23 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: let
-    forEachSystem = func:
-      nixpkgs.lib.genAttrs systems func;
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-  in rec {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
+  outputs = { self, nixpkgs }: let
+    forEachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in rec {
     packages = forEachSystem (system: {
-      bagnoster = with nixpkgs.legacyPackages.x86_64-linux;
+      bagnoster = with nixpkgs.legacyPackages.${system};
         stdenv.mkDerivation rec {
           name = "bagnoster";
           version = if self ? rev then self.rev else "dirty";
           bagnoster = ./bagnoster.bash;
           gitPath = git;
-          busyboxPath = busybox;
+          coreutilsPath = coreutils;
           builder = builtins.toFile "builder.sh" ''
             source $stdenv/setup
             cat $bagnoster |\
-              sed "s,git,$gitPath/bin/git,g;s,id -u,$busyboxPath/bin/id -u,;s,basename,$busyboxPath/bin/basename,g"\
+              sed "s,git,$gitPath/bin/git,g;s,id -u,$coreutilsPath/bin/id -u,;s,basename,$coreutilsPath/bin/basename,g"\
               >$out
             chmod 0555 $out
           '';
